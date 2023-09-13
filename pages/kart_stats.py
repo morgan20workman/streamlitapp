1cdf984337740cbd772962fd35d8e7e3a5edc8bf
morgan20workman <143902928+morgan20workman@users.noreply.github.com>
2023-09-13 18:06:08 +0000

    committing changes

--- pages/kart_stats.py
+++ pages/kart_stats.py
@@ -1,6 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.markdown("# Kart Configurations 🏎️")
 st.sidebar.markdown("# Kart Configurations 🏎️")
 
 st.write("What Kart Configuration is Best?")
+df_kart = pd.read_csv('data/kart_stats.csv')
+
+st.dataframe(df_kart)
